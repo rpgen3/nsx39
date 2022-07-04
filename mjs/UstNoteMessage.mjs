6d80a85edb04689197a9822e6210475c3c1decd5
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-04 23:42:19 +0900

    Update UstNoteMessage.mjs

--- mjs/UstNoteMessage.mjs
+++ mjs/UstNoteMessage.mjs
@@ -1,17 +1,18 @@
 import {MidiNoteMessage} from 'https://rpgen3.github.io/piano/mjs/midi/MidiNoteMessage.mjs';
 export class UstNoteMessage extends MidiNoteMessage {
-    constructor({pitch, velocity, when, lyric}) {
-        this.super({pitch, velocity, when});
+    constructor({ch, pitch, velocity, when, lyric}) {
+        this.super({ch, pitch, velocity, when});
         this.lyric = lyric;
     }
     static makeArray(ustNoteArray) {
         const result = [];
-        for (const {pitch, velocity, start, end, lyric} of ustNoteArray) {
+        for (const {ch, pitch, velocity, start, end, lyric} of ustNoteArray) {
             for (const [i, v] of [
                 start,
                 end
             ].entries()) {
                 result.push(new this({
+                    ch,
                     pitch,
                     velocity: i === 0 ? velocity : 0,
                     when: v,
