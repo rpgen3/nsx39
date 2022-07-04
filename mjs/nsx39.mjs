d1c46d9941c92032cdf170269aebf5e30a74cbfa
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-05 06:07:58 +0900

    Update nsx39.mjs

--- mjs/nsx39.mjs
+++ mjs/nsx39.mjs
@@ -13,24 +13,24 @@ export const nsx39 = new class {
         if (!nsx39) throw 'NSX-39 is not found.';
         this.midi = nsx39;
     }
-    noteOn({ch, pitch, velocity}) {
-        this.midi.send([0x90 + ch, pitch, velocity]);
+    noteOn(timestamp, {ch, pitch, velocity}) {
+        this.midi.send([0x90 + ch, pitch, velocity], timestamp);
     }
-    programChange({ch, program}) {
-        this.midi.send([0xC0 + ch, program])
+    programChange(timestamp, {ch, program}) {
+        this.midi.send([0xC0 + ch, program], timestamp)
     }
-    allSoundOff() {
+    allSoundOff(timestamp) {
         for (let i = 0; i < 0x10; i++) {
-            this.midi.send([0xB0 + i, 0x78, 0x00]);
+            this.midi.send([0xB0 + i, 0x78, 0x00], timestamp);
         }
     }
-    sendSysEx(data) {
-        this.midi.send([0xF0, 0x43, 0x79, 0x09, 0x11, ...data, 0xF7]);
+    sendSysEx(timestamp, data) {
+        this.midi.send([0xF0, 0x43, 0x79, 0x09, 0x11, ...data, 0xF7], timestamp);
     }
-    setLyric(lyric) {
+    setLyric(timestamp, lyric) {
         if (lyric in this.nsx39TextMap) {
             const lyricId = this.nsx39TextMap[lyric];
-            this.sendSysEx([0x0A, 0x00, lyricId]);
+            this.sendSysEx(timestamp, [0x0A, 0x00, lyricId]);
         }
     }
 }();
