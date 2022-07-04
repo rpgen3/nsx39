77a49069cbcfdba8e85744208db685ea828294c8
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-05 06:43:41 +0900

    Update and rename getUstTempos.mjs to UstTempoMessage.mjs

---
+++ mjs/UstTempoMessage.mjs
@@ -0,0 +1,15 @@
+import {MidiTempoMessage} from 'https://rpgen3.github.io/piano/mjs/midi/MidiTempoMessage.mjs';
+export class UstTempoMessage extends MidiTempoMessage {
+    static makeArray(ustEventArray) {
+        const result = [];
+        let currentTime = 0;
+        for(const {length, tempo} of ustEventArray) {
+            if (tempo !== null) result.push(new this({
+                tempo,
+                when: currentTime
+            }));
+            currentTime += length || 0;
+        }
+        return result;
+    }
+}
