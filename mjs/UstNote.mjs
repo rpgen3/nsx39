d1b85554868e985f687898abebe9fde2808cfb1a
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-04 20:59:16 +0900

    Update UstNote.mjs

--- mjs/UstNote.mjs
+++ mjs/UstNote.mjs
@@ -4,29 +4,25 @@ export class UstNote extends MidiNote {
         super({ch, pitch, velocity, start, end});
         this.lyric = lyric;
     }
-    static lyricRest = 'R';
-    static makeArray(ust, ch = 0) {
+    static makeArray(ustEventArray, ch = 0) {
         const result = [];
         let currentTime = 0;
-        for (const str of ust.split(/\[#[0-9]{4}\]/)) {
-            const m = new Map(str.split(/[\n\r]+/).map(v => v.split('=')).filter(v => v.length === 2));
-            const length = Number(m.get('Length'));
-            if (Number.isNaN(length)) throw 'The value of "Length" is NaN.';
-            const noteNum = Number(m.get('NoteNum'));
-            if (Number.isNaN(noteNum)) throw 'The value of "NoteNum" is NaN.';
-            const intensity = Number(m.get('Intensity'));
-            if (Number.isNaN(intensity)) throw 'The value of "Intensity" is NaN.';
-            const lyric = m.get('Lyric');
-            const end = currentTime + length;
-            const ustNote = new UstNote({
-                ch,
-                pitch: noteNum,
-                velocity: intensity,
-                start: currentTime,
-                end,
-                lyric
-            });
-            result.push(ustNote);
+        for (const {length, noteNum, lyric, intensity} of ustEventArray) {
+            const end = currentTime + (length || 0);
+            if (
+                noteNum !== null &&
+                lyric !== null &&
+                intensity !== null
+            ) {
+                result.push(new this({
+                    ch,
+                    pitch: noteNum,
+                    velocity: intensity,
+                    start: currentTime,
+                    end,
+                    lyric
+                }));
+            }
             currentTime = end;
         }
         return result;
