469bb0d5be058b1a861eda7c79160573baffdb05
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-05 11:55:30 +0900

    Update nsx39.mjs

--- mjs/nsx39.mjs
+++ mjs/nsx39.mjs
@@ -1,36 +1,60 @@
 import {nsx39TextMap} from 'https://rpgen3.github.io/nsx39/mjs/nsx39TextMap.mjs';
 export const nsx39 = new class {
     constructor() {
-        this.midi = null;
+        this.midiOutput = null;
         this.nsx39TextMap = nsx39TextMap;
     }
     async requestMIDIAccess() {
-        const midi = await navigator.requestMIDIAccess({
+        const midiAccess = await navigator.requestMIDIAccess({
             sysex: true,
             software: true
         });
-        const nsx39 = [...midi.outputs].map(([_, v]) => v).find(v => v.name === 'NSX-39 ');
+        const nsx39 = [...midiAccess.outputs].map(([_, v]) => v).find(name => ({name}) === 'NSX-39 ');
         if (!nsx39) throw 'NSX-39 is not found.';
-        this.midi = nsx39;
+        this.midiOutput = nsx39;
     }
-    noteOn(timestamp, {ch, pitch, velocity}) {
-        this.midi.send([0x90 | ch, pitch, velocity], timestamp);
+    send({data, timestamp}) {
+        this.midiOutput.send(data, timestamp);
     }
-    programChange(timestamp, {ch, programChange}) {
-        this.midi.send([0xC0 | ch, programChange], timestamp)
+    noteOn({data: {ch, pitch, velocity}, timestamp}) {
+        this.send({
+            data: [0x90 | ch, pitch, velocity],
+            timestamp
+        });
     }
-    allSoundOff(timestamp) {
+    programChange({data: {ch, programChange}, timestamp}) {
+        this.send({
+            data: [0xC0 | ch, programChange],
+            timestamp
+        });
+    }
+    soundOff({data: {ch}, timestamp}) {
+        this.send({
+            data: [0xB0 | ch, 0x78, 0x00],
+            timestamp
+        });
+    }
+    allSoundOff({timestamp}) {
         for (let i = 0; i < 0x10; i++) {
-            this.midi.send([0xB0 | i, 0x78, 0x00], timestamp);
+            this.soundOff({
+                data: {ch: i},
+                timestamp
+            });
         }
     }
-    sendSysEx(timestamp, data) {
-        this.midi.send([0xF0, 0x43, 0x79, 0x09, 0x11, ...data, 0xF7], timestamp);
+    sendSysEx({data, timestamp}) {
+        this.send({
+            data: [0xF0, 0x43, 0x79, 0x09, 0x11, ...data, 0xF7],
+            timestamp
+        });
     }
-    setLyric(timestamp, lyric) {
+    setLyric({data: {lyric}, timestamp}) {
         if (lyric in this.nsx39TextMap) {
             const lyricId = this.nsx39TextMap[lyric];
-            this.sendSysEx(timestamp, [0x0A, 0x00, lyricId]);
+            this.sendSysEx({
+                data: [0x0A, 0x00, lyricId],
+                timestamp
+            });
         }
     }
 }();
