6d45f53107576f47863b166e750aef2c00ec3c50
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-07-06 21:03:19 +0900

    Update tuning39.mjs

--- mjs/tuning39.mjs
+++ mjs/tuning39.mjs
@@ -1,11 +1,11 @@
 export const tuning39 = messages => {
     const noteOn = new Set;
-    for (const {when, channel, velocity = null} of messages) {
-        if (channel === 0 && velocity !== 0 && velocity !== null) noteOn.add(when); 
+    for (const {when, channel, velocity} of messages) {
+        if (channel === 0 && velocity !== 0) noteOn.add(when);
     }
     const excluded = new Set;
     for (const [i, {when, channel, velocity}] of messages.entries()) {
-        if (channel === 0 && velocity === 0 && noteOn.has(when)) excluded.add(i); 
+        if (channel === 0 && velocity === 0 && noteOn.has(when)) excluded.add(i);
     }
-    return messages.filter((_, i) => excluded.has(i));
+    return messages.filter((_, i) => !excluded.has(i));
 };
