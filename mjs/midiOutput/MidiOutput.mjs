8d2877ce0720e53212f50329479a4f31cf94238a
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-12-13 21:32:43 +0900

    Update MidiOutput.mjs

--- mjs/midiOutput/MidiOutput.mjs
+++ mjs/midiOutput/MidiOutput.mjs
@@ -1,6 +1,15 @@
 export class MidiOutput {
     constructor () {
         this.midiOutput = null;
+        this.allChannels = new Proxy(this, {
+            get(target, prop) {
+                return ({data, timestamp}) => {
+                    for (let i = 0; i < 0x10; i++) {
+                        target[prop]({data: {channel: i, ...data}, timestamp});
+                    }
+                };
+            }
+        });
     }
     async fetchMidiOutputs() {
         const midiAccess = await navigator.requestMIDIAccess({
@@ -12,30 +21,39 @@ export class MidiOutput {
     send({data, timestamp}) {
         this.midiOutput.send(data, timestamp);
     }
+    // 鍵盤を押下
     noteOn({data: {channel, pitch, velocity}, timestamp}) {
         this.send({
             data: [0x90 | channel, pitch, velocity],
             timestamp
         });
     }
+    // 音色の切り替え
     programChange({data: {channel, programChange}, timestamp}) {
         this.send({
             data: [0xC0 | channel, programChange],
             timestamp
         });
     }
-    soundOff({data: {channel}, timestamp}) {
+    // 発音中の音や残響を即時に停止
+    allSoundOff({data: {channel}, timestamp}) {
         this.send({
             data: [0xB0 | channel, 0x78, 0x00],
             timestamp
         });
     }
-    allSoundOff({timestamp} = {}) {
-        for (let i = 0; i < 0x10; i++) {
-            this.soundOff({
-                data: {channel: i},
-                timestamp
-            });
-        }
+    // 音色の初期化
+    resetAllControllers({data: {channel}, timestamp}) {
+        this.send({
+            data: [0xB0 | channel, 0x79, 0x00],
+            timestamp
+        });
+    }
+    // 発音中の音をノートオフ
+    allNotesOff({data: {channel}, timestamp}) {
+        this.send({
+            data: [0xB0 | channel, 0x7B, 0x00],
+            timestamp
+        });
     }
 };
